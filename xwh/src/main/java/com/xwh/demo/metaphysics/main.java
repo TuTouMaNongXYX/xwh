3e0120ffeb59f9c111e9ee2369573694ef3ab4d7
xxl197897 <3390380439@qq.com>
2022-06-22 11:08:30 +0800

    接入命理玄学知识图谱-观音灵签

---
+++ xwh/src/main/java/com/xwh/demo/metaphysics/main.java
@@ -0,0 +1,63 @@
+package com.xwh.demo.metaphysics;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: 谢宇轩 命理玄学知识图谱-观音灵签 奇奇怪怪的玄学
+ * @Description: TODO
+ * @DateTime: 2022/6/22 10:51
+ **/
+public class main {
+    //API产品路径
+    static String host = "http://gylq.market.alicloudapi.com";
+    static String path = "/ai_metaphysics/guan_yin_lin_qian/elite";
+    static String method = "GET";
+    //阿里云APPCODE
+    static String appcode = "7cbc1500dfd34d91bc1728aa9ef29785";
+
+    public static void fortuneTelling(String timeOfBirth, String familyName, String lastName, String sex) {
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Authorization", "APPCODE " + appcode);
+        //UUID采用当前程序运行时间，用于防止重放攻击，开发者可根据自己需求，自定义字符串
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String UUID = df.format(new Date());
+        headers.put("X-Ca-Nonce", UUID);
+        Map<String, String> querys = new HashMap<String, String>();
+        //参数配置
+        //生日，如：20180808080808，即2018年08月08日08时08分08秒，若无法确定确切小时分钟与秒，请用00表示，如：20180808000000
+        querys.put("BIRTH", timeOfBirth);
+        //姓氏，如：张
+        querys.put("FIRST_NAME", familyName);
+        //性别，如：男
+        querys.put("GENDER", sex);
+        //名称，如：无忌
+        querys.put("LAST_NAME", lastName);
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doGet(host, path, method, headers, querys);
+            System.out.println(response.toString());
+            //获取response的body
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
--- xwh/src/test/java/com/xwh/demo/DemoApplicationTests.java
+++ xwh/src/test/java/com/xwh/demo/DemoApplicationTests.java
@@ -3,6 +3,7 @@ package com.xwh.demo;
 import com.xwh.demo.arcsoft.FaceEngineMain;
 import com.xwh.demo.cloud.alibaba.identityCardVerification.verification;
 import com.xwh.demo.cloud.tencent.shortMessage;
+import com.xwh.demo.metaphysics.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,8 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() throws IOException {
-        System.out.println( verification.is("430481200201229159","谢宇轩"));
+        main.fortuneTelling("20010128000000","谢","宇轩","男");
+
     }
 
 
